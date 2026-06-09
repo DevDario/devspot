bd0c3f939d688564034076e24a50fd7484f33a9d
DevDario <dariosilva13222@gmail.com>
2026-06-09 14:18:18 +0000

    feat: wire up App entry point with BrowserRouter and AuthProvider
    - main.tsx: render App in StrictMode via createRoot
    - App.tsx: BrowserRouter wrapping AuthProvider with 5 routes
      - / -> Home (map)
      - /place/:id -> PlaceDetailPage
      - /profile/:username -> ProfilePage
      - /signin -> SignInPage
      - /signup -> SignUpPage

---
+++ src/App.tsx
@@ -0,0 +1,23 @@
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import { AuthProvider } from '@/lib/supabase/auth'
+import { Home } from '@/pages/Home'
+import { PlaceDetailPage } from '@/pages/PlaceDetailPage'
+import { ProfilePage } from '@/pages/ProfilePage'
+import { SignInPage } from '@/pages/SignInPage'
+import { SignUpPage } from '@/pages/SignUpPage'
+
+export default function App() {
+  return (
+    <BrowserRouter>
+      <AuthProvider>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/place/:id" element={<PlaceDetailPage />} />
+          <Route path="/profile/:username" element={<ProfilePage />} />
+          <Route path="/signin" element={<SignInPage />} />
+          <Route path="/signup" element={<SignUpPage />} />
+        </Routes>
+      </AuthProvider>
+    </BrowserRouter>
+  )
+}
